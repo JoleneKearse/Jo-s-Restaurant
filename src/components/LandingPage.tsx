28f81c3fb54c80546bd2ba82ca5f90bf7e290e0f
Jolene Kearse <pharaohnutz@gmail.com>
2023-06-10 17:23:42 -0400

    feat: add LandingPage & utility classes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,14 +2,16 @@ import hero from "../assets/hero.png";
 
 const LandingPage = () => {
   return (
-    <div className="landingPage">
-      <h1>New York's Favorite Burger</h1>
-      <p>
-        In a world so painfully serious, yet so ridiculous, you knew it was only
-        a matter of time before plant-based steak became a thing.
-      </p>
-      <img src={hero} alt="Jo's Restaurant world famous Shrimp Yo burger" />
-      <button>Browse Menu</button>
+    <div className="accent-bg flex">
+      <div className="container">
+        <h1 className="text-uppercase">New York's Favorite Burger</h1>
+        <p>
+          In a world so painfully serious, yet so ridiculous, you knew it was
+          only a matter of time before plant-based steak became a thing.
+        </p>
+        <img src={hero} alt="Jo's Restaurant world famous Shrimp Yo burger" />
+        <button>Browse Menu</button>
+      </div>
     </div>
   );
 };
